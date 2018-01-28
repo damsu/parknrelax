1a0b3bf5cf91a3aa4880a13e1158ed2984e80ea0
damsu <damorizur@gmail.com>
2018-01-28 21:53:33 +0200

    Basic homescreen and navigation

--- ParknRelax/App.js
+++ ParknRelax/App.js
@@ -1,15 +1,87 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Park|n|Relax'
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <View style={{marginTop: 25}}>
+		<Button
+          onPress={() => navigate('Login')}
+          title="Sign In"
+        />
+		</View>
+		<View style={{marginTop: 25}}>
+		<Button
+          onPress={() => navigate('SignUp')}
+          title="Sign Up"
+        />
+		</View>
+		<View style={{marginTop: 25}}>
+		<Button
+          onPress={() => navigate('Available Parking Spots')}
+          title="Available Parking Spots"
+        />
+		</View>
+      </View>
+    );
+  }
+}
+
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+}
+
+class SignUpScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Sign Up',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Sign Up</Text>
+      </View>
+    );
+  }
+}
+
+class ListScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Available Parking Spots',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Available Parking Spots</Text>
+      </View>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Login: { screen: LoginScreen },
+  SignUp: { screen: SignUpScreen },
+  List: { screen: ListScreen },
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <SimpleApp />;
   }
 }
 
@@ -18,6 +90,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  
+  button: {
+	  width: 150, height: 150, backgroundColor: 'steelblue'
+  }
 });
